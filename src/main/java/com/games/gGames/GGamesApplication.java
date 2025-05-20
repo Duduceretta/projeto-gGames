a894b9e77c457ca9c33acb842523773d7dc38d54
Eduardo Ceretta <duduceretta@gmail.com>
2025-05-20 17:50:35 -0300

    Project created

---
+++ src/main/java/com/games/gGames/GGamesApplication.java
@@ -0,0 +1,13 @@
+package com.games.gGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GGamesApplication.class, args);
+	}
+
+}
